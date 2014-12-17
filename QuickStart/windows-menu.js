534e82030c61277ebc7c34b0dd56f101e44e52b2
Ken Wilcox <wilcoxk@gmail.com>
2014-12-17 14:32:29 -0700

    quick start

---
+++ QuickStart/windows-menu.js
@@ -0,0 +1,25 @@
+// Windows menu
+var gui = require('nw.gui');
+var win = gui.Window.get();
+var menubar = new gui.Menu({
+  type: 'menubar'
+});
+
+var file = new gui.Menu();
+file.append(new gui.MenuItem({
+  label: 'Action 1',
+  click: function() {
+    alert('Action 1 Clicked');
+  }
+}));
+
+file.append(new gui.MenuItem({
+  label: 'Action 2',
+  click: function() {
+    alert('Action 2 Clicked');
+  }
+}));
+
+menubar.append(new gui.MenuItem({label: 'File', submenu: file}));
+
+win.menu = menubar;
